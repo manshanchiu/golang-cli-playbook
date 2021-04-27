e279898ac914eda371bc2f0605d04d889a456ac0
roy chiu <manshan.chiu@grabtaxi.com>
2021-04-27 12:53:34 +0800

    Completed module

--- go.mod
+++ go.mod
@@ -3,6 +3,5 @@ module github.com/pluralsight-projects/golang-cli-playbook
 go 1.13
 
 require (
-	github.com/nathan-osman/go-sunrise v0.0.0-20201029015502-9a83cd1a5746
 	golang.org/x/tools v0.0.0-20200214225126-5916a50871fb // indirect
 )
--- module3/main.go
+++ module3/main.go
@@ -2,15 +2,8 @@ package module3
 
 import (
 	"fmt"
-	"time"
-
-	"github.com/nathan-osman/go-sunrise"
 )
 
 func main() {
 	fmt.Println("hello world")
-	sunrise.SunriseSunset(
-		43.65, -79.38, // Toronto, CA
-		2000, time.January, 1, // 2000-01-01
-	)
 }
