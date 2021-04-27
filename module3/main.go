bbc0a9bc390e477a42116db56cd7c855711862ad
roy chiu <manshan.chiu@grabtaxi.com>
2021-04-27 12:50:36 +0800

    Completed module

--- module3/main.go
+++ module3/main.go
@@ -1,7 +1,16 @@
 package module3
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/nathan-osman/go-sunrise"
+)
 
 func main() {
 	fmt.Println("hello world")
+	sunrise.SunriseSunset(
+		43.65, -79.38, // Toronto, CA
+		2000, time.January, 1, // 2000-01-01
+	)
 }
